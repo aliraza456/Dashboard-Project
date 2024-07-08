845d3427910652be2bc52ae545e705c37325db76
aliraza456 <alirazasharifrajpoot@gmail.com>
2024-07-08 12:07:28 +0100

    Charts has been added

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Header from "../../components/Header";
+import { LineChart } from "../../components/Index";
 const Line = () => {
   return (
-    <div>Line</div>
-  )
-}
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
 
-export default Line
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
+};
+
+export default Line;
